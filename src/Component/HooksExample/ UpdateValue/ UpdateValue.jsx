4852609e985b70d9d02f04c8b4317d5be49c6d91
developer-Pavithra <developerpavithra91@gmail.com>
2022-01-04 14:18:36 +0530

    1 file modified

--- src/Component/HooksExample/ UpdateValue/ UpdateValue.jsx
+++ src/Component/HooksExample/ UpdateValue/ UpdateValue.jsx
@@ -1,4 +1,34 @@
 import React from 'react';
 export function UpdateValue() {
-  return <div></div>;
+  const [details, setDetails] = useState({
+    name: 'Yazhini',
+    age: 5,
+  });
+  let handleChange = (e) => {
+    let { name, value } = e.target;
+    setDetails({ ...details, [name]: value });
+  };
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    alert(details.name + ' ' + details.age + '  submitted');
+  };
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          value={details.name}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Enter Age"
+          value={details.age}
+          onChange={handleChange}
+        />
+        <button type="submit"> Submit </button>
+      </form>
+    </div>
+  );
 }
