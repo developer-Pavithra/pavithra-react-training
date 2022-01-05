99cf91ec9754aeb3fc38532ecd923f2caab29c45
developer-Pavithra <developerpavithra91@gmail.com>
2022-01-05 16:59:00 +0530

    2 files modified, 1 added

--- src/Component/HooksExample/Hooks/Hooks.jsx
+++ src/Component/HooksExample/Hooks/Hooks.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { UpdateValue } from '../../../Component/HooksExample/ UpdateValue/ UpdateValue';
+import { Lifecycle } from '../../../Component/HooksExample/Lifecycle/Lifecycle';
 import './Hooks.css';
 
 export function Hooks() {
-  return <div className="fun-content"></div>;
+  return (
+    <div className="fun-content">
+      <h4>Function Component</h4>
+      <h5>
+        (stateless componenthooks feature react16+ some addition feature
+        -useState,useEffect,useCallback,useRef,useDispatch etc..,) without
+        writing a class
+      </h5>
+      <ol>
+        <li>
+          <em>useState</em>
+          <UpdateValue />
+        </li>
+        <li>
+          <em>useEffect</em>
+          <Lifecycle />
+        </li>
+      </ol>
+    </div>
+  );
 }
