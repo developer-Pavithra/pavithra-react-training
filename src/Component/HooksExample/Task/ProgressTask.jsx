03799f4356fe8f85406293d65100e04e35fc13b7
developer-Pavithra <developerpavithra91@gmail.com>
2022-01-07 17:50:40 +0530

    4 files modified, 1 added

--- src/Component/HooksExample/Task/ProgressTask.jsx
+++ src/Component/HooksExample/Task/ProgressTask.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { CompletedTask } from '../../../Component/HooksExample/Task/CompletedTask';
-export function ProgressTask() {
+export function ProgressTask(props) {
   return (
     <div>
       <hr />
-      <h3>ProgressTask</h3>
-      <CompletedTask />
+      <h3>ProgressTask </h3>
+      <CompletedTask user={props.user} />
     </div>
   );
 }
--- src/Component/HooksExample/Task/StartTask.jsx
+++ src/Component/HooksExample/Task/StartTask.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { ProgressTask } from '../../../Component/HooksExample/Task/ProgressTask';
-export function StartTask() {
+export function StartTask(props) {
   return (
     <div>
       <hr />
       <h3>StartTask</h3>
-      <ProgressTask />
+      <ProgressTask user={props.user} />
     </div>
   );
 }
