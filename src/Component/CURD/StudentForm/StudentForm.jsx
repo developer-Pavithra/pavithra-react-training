0f84eb31889f44eb26e3aa12ebd41c2c331cb9a7
developer-Pavithra <developerpavithra91@gmail.com>
2022-01-11 14:15:39 +0530

    2 files modified, 1 added

--- src/Component/CURD/StudentForm/StudentForm.jsx
+++ src/Component/CURD/StudentForm/StudentForm.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StudentTable } from '../../../Component/CURD/StudentTable/StudentTable';
 import { UpdateValue } from '../../../Component/HooksExample/ UpdateValue/ UpdateValue';
+import './StudentForm.css';
 export function StudentForm() {
   useEffect(() => {}, []);
   return (
-    <div>
+    <div className="curd-op">
       <h4>Student Details</h4>
       <UpdateValue />
       <StudentTable />
