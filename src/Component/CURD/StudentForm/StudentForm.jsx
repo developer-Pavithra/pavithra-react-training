fc89b9c2d67057ea4871e009994a90829d9136e1
developer-Pavithra <developerpavithra91@gmail.com>
2022-01-11 14:20:01 +0530

    2 files modified, 1 added

--- src/Component/CURD/StudentForm/StudentForm.jsx
+++ src/Component/CURD/StudentForm/StudentForm.jsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
-import { StudentTable } from '../../../Component/CURD/StudentTable/StudentTable';
-import { UpdateValue } from '../../../Component/HooksExample/ UpdateValue/ UpdateValue';
+import React from 'react';
 import './StudentForm.css';
 export function StudentForm() {
-  useEffect(() => {}, []);
   return (
     <div className="curd-op">
       <h4>Student Details</h4>
-      <UpdateValue />
-      <StudentTable />
     </div>
   );
 }
--- src/Component/CURD/StudentTable/StudentTable.jsx
+++ src/Component/CURD/StudentTable/StudentTable.jsx
@@ -1,40 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 export function StudentTable() {
-  const [details, setDetails] = useState([]);
-  useEffect(() => {
-    axios
-      .get('https://61dd3daaf60e8f0017668688.mockapi.io/student')
-      .then((response) => {
-        console.log('response', response);
-        setDetails(response.data);
-      });
-  }, []);
-
-  let handleDelete = (id) => {
-    axios
-      .delete('https://61dd3daaf60e8f0017668688.mockapi.io/student' + id)
-      .then((response) => {
-        console.log('response', response);
-      });
-  };
   return (
     <div>
       <h4>Student Table</h4>
-      {details.length > 0
-        ? details.map((item) => {
-            return (
-              <tbody>
-                <tr id={item.id}>
-                  <td>{item.name}</td>
-                  <td>
-                    <button onClick={()=>handleDelete(item.id)}>Delete</button>
-                  </td>
-                </tr>
-              </tbody>
-            );
-          })
-        : ''}
     </div>
   );
 }
